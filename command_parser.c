8b22f65a518dc8d9b94126bbcc8acbd4449d567b
matteo113 <besancon.matteo@gmail.com>
2017-10-31 14:04:07 +0100

    add info for tp4

---
+++ command_parser.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "command_parser.h"
+
+int parseCommand( const char *command, Command *result ) {
+//your code here
+//return -1
+//return 0; 
+}
+
---
+++ command_parser.h
@@ -0,0 +1,8 @@
+typedef struct Command_t{
+//Create your structure here.
+} Command;
+
+
+typedef struct Command_t Command;
+int parseCommand( const char* input, Command *result );
+
