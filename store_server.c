65449830e3a6dcb8cf113115309715af1c29a128
terryvogelsang <terry.vogelsang@protonmail.com>
2017-12-18 16:45:52 +0100

    Fix parsing

--- store_server.c
+++ store_server.c
@@ -69,61 +69,67 @@ void freeFDState(FDState *state) {
 	free(state);
 }
 
-FDState *states[FD_SETSIZE]; //state container as described in TP7 giveaway
-//===========================//
-
-
-
-
-int listener; //the main listener file descriptor for the server socket - non blocking property for all connections derived from this one after you set it.
+FDState *states[FD_SETSIZE]; 
+int listener; 
 Store *store;
 
-void setNonBlocking(int fd){
-  //wrap around fcntl here
-  //1) get the file descriptor flags
-  //2) update the file descriptor to support non-blocking access - refer to manual for return values (EAGAIN EWOULDBLOCK)
-	int flags = fcntl(fd, F_GETFL, 0);
-	fcntl(fd, F_SETFL, O_NONBLOCK);
+void setNonBlocking(int fd) {
+
+  int flags = fcntl (fd, F_GETFL);
+
+  // If reading the flags fails return error (EAGAIN == EWOULDBLOCK)
+  if (flags == EAGAIN) {
+    return -1;
+  }
+  // Set O_NONBLOCK Flag
+  flags |= O_NONBLOCK;
+
+  // Write the new flag (EAGAIN == EWOULDBLOCK)
+  if (fcntl (fd, F_SETFL, flags) == EAGAIN) {
+    return -1;
+  }
+ 
 }
 
 int receiveCommand(int fd, FDState *state)
 {
-	//read from socket here
-	//return 0 for success
-	//return -1 for failure
 	int nread;
 	nread = read(fd, state->buffer, BUF_LEN);
-	/*if(nread  0){
-		die("wow something went wrong...");
-	}*/
-	if(nread <= 0){
-		if(errno == EAGAIN){
+
+	if(nread <= 0) 
+	{
+		if(errno == EAGAIN)
+		{
 			return 0;
 		}
-		else{
+		else
+		{
 			perror("Read error");
 			return -1;
 		}
 	}
-	else{
+	else
+	{
 		state->status = WRITING;
-		state->end = nread; // wtf ??
+		state->end = nread - 1;
 		return 0;
 	}
 }
 
 int sendResult( int fd, FDState *state, Store *store )
 {
-	//write to socket here
-	// return 0 for success
-	//return -1 for failure
 	int nwrite;
-	char * command = malloc (state->end);
-	strncpy(command, state->buffer, state->end);
-	char * rep = runCommand(store, state->buffer);
+
+	char * command = malloc (state->end + 1);
+	strcpy(command, state->buffer);
+	command[strlen(command)-1] = '\0';
+
+	printf("Command to send : --%s--", command);
+
+	char * rep = runCommand(store, command);
 	//state->status = RESULT;
-	nwrite = write(fd, rep, sizeof(rep));
-	if (nwrite != sizeof(rep)){
+	nwrite = write(fd, rep, strlen(rep));
+	if (nwrite != strlen(rep)){
 		if (errno == EAGAIN){
 			return 0;
 		}
@@ -132,7 +138,7 @@ int sendResult( int fd, FDState *state, Store *store )
 			return -1;
 		}
 	}
-	else{
+	else {
 		state->status = DONE;
 	}
 }
@@ -142,7 +148,6 @@ void run( int port )
 {
 	//here is where the magic happens.
 	//handle ALL ERRORS
-	int listener;
 	int i, maxfd;
 	struct sockaddr_in sin;
 	fd_set readset, writeset;
@@ -160,6 +165,13 @@ void run( int port )
 	// make listener socket, set it non blocking and bind it
 	listener = socket(AF_INET, SOCK_STREAM, 0);
 	setNonBlocking(listener);
+
+	int y = 1;
+    if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &y, sizeof(y)) == -1) 
+    {
+        perror("[-] setsockopt error ! ...");
+        exit(EXIT_FAILURE);
+    }
 	if (bind(listener, (struct sockaddr*) &sin, sizeof(sin))<0){
 		die("Bind error");
 	}
@@ -174,71 +186,84 @@ void run( int port )
 		maxfd = listener;
 		FD_ZERO(&readset);
 		FD_ZERO(&writeset);
-
 		FD_SET(listener, &readset);
 
-		// update FD sets
-		for (i=0; i < FD_SETSIZE; ++i) {
-      if (states[i]) {
-        if (i > maxfd)
-	   			maxfd = i;
-	 			FD_SET(i, &readset);
-	 			if( states[i]->status == WRITING) {
-	   			FD_SET(i, &writeset);
-	 			}
-      }
-    }
 
+		// update FD sets
+		for (i=0; i < FD_SETSIZE; ++i) 
+		{
+			if (states[i]) 
+			{
+				if (i > maxfd) 
+				{
+					maxfd = i;
+					FD_SET(i, &readset);
+
+					if( states[i]->status == WRITING) 
+					{	
+						FD_SET(i, &writeset);
+					}
+				}
+      		}
+    	}
+		
 		// performing the select operation
 		if(select(maxfd+1, &readset, &writeset, NULL, NULL)<0) {
 			die("Select error");
 		}
 
 		// Accept new connections
-		if (FD_ISSET(listener, &readset)){
+		if (FD_ISSET(listener, &readset)) {
 			int fd;
-      struct sockaddr_storage ss;
-      socklen_t slen = sizeof(ss);
+			struct sockaddr_storage ss;
+			socklen_t slen = sizeof(ss);
 			fd = accept(listener, (struct sockaddr*)&ss, &slen);
-
-			if (fd < 0){
+			if (fd < 0)
+			{
 				die("Accept error");
 			}
-			else if (fd > FD_SETSIZE){
+			else if (fd > FD_SETSIZE)
+			{
 				close(fd);
 			}
-			else{
+			else
+			{
 				printf("Accepted connection %d\n", fd);
 				setNonBlocking(fd);
-				states[fd] = allocFDState;
+				states[fd] = allocFDState();
 			}
 		}
 
 		// do things ...
-		for(i=0; i<maxfd; i++){
+		for(i=0; i < maxfd; i++)
+		{
 			int r = 0;
-      if (i == listener) continue;
-      if (FD_ISSET(i, &readset)) {
+      		if (i == listener) continue;
+      		if (FD_ISSET(i, &readset))   
+			{
 				printf( "Reading from %d\n" , i );
 				r = receiveCommand(i, states[i]);
-      }
+				printf("Received CMD : %s", states[i]->buffer);
+      		}
 
-			if (r == 0 && FD_ISSET(i, &writeset)) {
+			if (r == 0 && FD_ISSET(i, &writeset)) 
+			{
 				printf( "Writing to %d\n" , i );
 				r = sendResult(i, states[i], store);
 
-				if ( states[i]->status == DONE ) {
-		  		printf( "Closing socket: %d\n", i );
-		  		freeFDState(states[i]);
-		  		states[i] = NULL;
-		  		close(i);
+				if ( states[i]->status == DONE ) 
+				{
+					printf( "Closing socket: %d\n", i );
+					freeFDState(states[i]);
+					states[i] = NULL;
+					close(i);
 				}
-      }
+      		}
 		}
 	}
 }
 
-//																	-------- TO DO ! -------
+// -------- TO DO ! -------
 void shutdownServer( int signum )
 {
 	//signal callback
